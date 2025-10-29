4fd0998b43477801dacf0b7448b967ca03341198
MateoJJaramilloS <mateojaramillosalinas@gmail.com>
2025-10-29 12:52:08 -0500

    1

---
+++ Ejemplo01/src/ejemplo01/Ejemplo061.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo061 {
+
+    public static void main(String[] args) {
+        // Uso de printf
+        // f: formato
+
+        //String nombreEstudiante = "René Rolando";
+        //String apellidoEstudiante = "Elizalde Solano";
+        //int nacimiento = 1983;
+        // System.out.println(nombreEstudiante+" "+apellidoEstudiante+ " "+ 
+        // nacimiento);
+        // En printf; 
+        // usamos %s para reemplazar las cadenas, enteros, decimales
+        // usamos %d para reemplazar los valores enteros
+        // usamos %f para reemplazar los valore decimales
+        // System.out.printf("%s\n\n%s\n\n%s\n", nombreEstudiante, 
+        //        apellidoEstudiante, nacimiento);
+        String nombre = "Mateo";
+        String apellido = "Jaramllo";
+        int edad = 17;
+        String direccion = "Daniel Alvarez";
+        String universidad = "UTPL";
+        String ciclo = "Primer Ciclo";
+        String asignatura = "Introduccion a la programación";
+        String paralelo = "A";
+
+        System.out.printf("Nombre:\n\t%s\nApellido:\n\t%s\nDirección:\n\n\t&s\nUniversidad:%s\nCiclo:%s\nAsignatura:%s\nParelo:\n\t%s\n",nombre, apellido,edad,direccion,universidad,ciclo,asignatura,paralelo);
+
+    }
+}
